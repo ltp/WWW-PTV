1d9e2b28c99296d7107cd2e02a488c780ff434c1
Luke Poskitt <luke.poskitt@gmail.com>
2014-04-17 07:09:03 +1000

    Added constructor, basic framework

--- lib/WWW/PTV/TimeTable.pm
+++ lib/WWW/PTV/TimeTable.pm
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+package WWW::PTV::TimeTable;
 
 use strict;
 use warnings;
@@ -8,4 +8,17 @@ use Carp qw(croak);
 our $VERSION = '0.01';
 
 our @ATTR = qw( route_id direction direction_desc name );
-our 
+
+sub new {
+	my($class, $stop_names, $stop_ids, $stop_times) = @_;
+	my $self = bless {}, $class;
+	$self->{stop_names} 	= $stop_names;
+	$self->{stop_ids}	= $stop_ids;
+	$self->{stop_times}	= $stop_times;
+	return $self
+}
+
+1;
+
+__END__
+
