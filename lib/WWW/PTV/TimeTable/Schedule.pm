e70d26e8a86df298845068399087a669d64d1c1c
Luke Poskitt <luke.poskitt@gmail.com>
2014-04-23 17:49:06 +1000

    Yeah, more documentation

--- lib/WWW/PTV/TimeTable/Schedule.pm
+++ lib/WWW/PTV/TimeTable/Schedule.pm
@@ -119,7 +119,61 @@ Returns the complete schedule as an ordered list.  Note that schedule times
 may not conform to a standard format (e.g. hh:mm) and may use ASCII or Unicode
 characters to indicate special values.
 
-=head3
-This
+=head1 AUTHOR
+
+Luke Poskitt, C<< <ltp at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-ptv-timetable-schedule at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-PTV-TimeTable-Schedule>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
 
 
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WWW::PTV::TimeTable::Schedule
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-PTV-TimeTable-Schedule>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-PTV-TimeTable-Schedule>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-PTV-TimeTable-Schedule>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-PTV-TimeTable-Schedule/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Luke Poskitt.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
