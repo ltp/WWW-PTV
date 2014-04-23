562e7d123e771d733e4db949a0f39eb20a214eca
Luke Poskitt <luke.poskitt@gmail.com>
2014-04-23 17:40:50 +1000

    Yeah documentation

--- lib/WWW/PTV/TimeTable/Schedule.pm
+++ lib/WWW/PTV/TimeTable/Schedule.pm
@@ -10,6 +10,10 @@ sub new {
 	return $self
 }
 
+sub as_list {
+	return @{ $_[0]->{schedule} };
+}
+
 sub next {
 	my ($self, $n) = @_;
 	$n or $n = 1;
@@ -32,7 +36,7 @@ sub next {
 		}
 	}
 
-	return undef
+	return \@res
 }
 
 sub next_five {
@@ -40,3 +44,82 @@ sub next_five {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+WWW::PTV::TimeTable::Schedule - Class for operations with PTV time table schedules.
+
+=head1 SYNOPSIS
+
+	use WWW::PTV;
+	my $ptv = WWW::PTV->new;
+	
+	# Get the next outbound departure time for route 1 from stop ID 19849
+	my $next = $ptv->get_route_by_id(1)
+		       ->get_outbound_tt
+		       ->get_schedule_by_stop_id(19849)
+		       ->next;
+	
+	print "The next service is scheduled at @$next\n";
+	
+	# Get the next five services for route 1 from the same stop.
+	my $next = $ptv->get_route_by_id(1)
+		       ->get_outbound_tt
+		       ->get_schedule_by_stop_id(19849)
+		       ->next_five;
+
+	# Print all 
+
+=head1 DESCRIPTION
+
+This module implements a utility class providing operations for PTV timetable 
+schedules.
+
+Please note the terminology used for the naming of this module should not imply
+relationships between routes, timetables, schedules and other objects.  In brief, 
+the relationships between objects defined in the WWW::PTV namespace are:
+
+	       1       *
+	route --- has ---> timetables
+
+	           1       *
+	timetable --- has ---> schedules
+
+	          1       *
+	schedule --- has ---> stops (service times)
+
+That is to say; a route may have one or more timetables (inbound, outbound,
+weekend, public holiday, etc.), each of which is composed of one or more
+schedules where a schedules is defined as the list of service times for a 
+particular stop on that route.
+
+=head2 METHODS
+
+=head3 next ( $INT )
+
+Returns the next chronological service time on the schedule in respect to
+the current local time.
+
+This method accepts an integer value as an optional parameter which, if passed
+specifies the number of the next n service times to return.
+
+	# Return the next three service times
+	my @next_three_trains = $timetable->next(3);
+
+=head3 next_five ()
+
+Returns the chronological next five service times for the schedule
+emulating the popular "next five" feature on the PTV website.
+
+=head3 as_list ()
+
+Returns the complete schedule as an ordered list.  Note that schedule times
+may not conform to a standard format (e.g. hh:mm) and may use ASCII or Unicode
+characters to indicate special values.
+
+=head3
+This
+
+
