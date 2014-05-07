7e501c709f2c808d9761228d01cd2144ce184179
Luke Poskitt <luke.poskitt@gmail.com>
2014-05-07 17:45:06 +1000

    Added methods to WWW::PTV::Area

--- lib/WWW/PTV/Area.pm
+++ lib/WWW/PTV/Area.pm
@@ -4,10 +4,28 @@ use strict;
 use warnings;
 
 sub new {
-	my ($class, $args) = @_;
+	my ($class, %args) = @_;
 	my $self = bless {}, $class;
-
+	$self->{id}	= $args{id};
+	$self->{name}	= $args{name};
+	$self->{suburbs}= $args{suburbs};
 	return $self
+}
+
+sub id {
+	return $_[0]->{id}
+}
+
+sub name {
+	return $_[0]->name
+}
+
+sub suburbs {
+	return $_[0]->{suburbs}
+}
+
+sub towns {
+	$_[0]->suburbs
 }
 
 1;
